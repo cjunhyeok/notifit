d509128db4bb665bab06103f666f373ba37d0a33
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-20 21:32:40 +0900

    Initialize Commit

---
+++ src/main/java/com/notifit/NotifitApplication.java
@@ -0,0 +1,13 @@
+package com.notifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifit/NotifitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
