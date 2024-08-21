eff001c39ae937a1dec25a8278b886cc2545b4f9
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-21 20:19:24 +0900

    Feat: 회원 Entity, 정적 팩토리 메서드 개발

---
+++ src/main/java/com/notifit/domain/member/entity/Member.java
@@ -0,0 +1,47 @@
+package com.notifit.domain.member.entity;
+
+import com.notifit.global.exception.CustomException;
+import com.notifit.global.exception.ErrorCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+    private String name;
+    private String phoneNumber;
+
+    private Member(String username, String password, String name, String phoneNumber) {
+
+        if (username == null || username.isEmpty()) {
+            throw new CustomException(ErrorCode.EMPTY_USERNAME);
+        }
+
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Member of(String username, String password, String name, String phoneNumber) {
+        return new Member(
+                username,
+                password,
+                name,
+                phoneNumber);
+    }
+}
