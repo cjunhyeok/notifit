6027fee2322604cbf17777b0b358c3e80e31d216
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-22 21:28:04 +0900

    Feat: 운동 Entity 개발

---
+++ src/main/java/com/notifit/domain/workout/entity/Workout.java
@@ -0,0 +1,31 @@
+package com.notifit.domain.workout.entity;
+
+import com.notifit.domain.workout.entity.enums.BodyPart;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Workout {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "workout_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private BodyPart bodyPart;
+    private String workoutName;
+
+    private Workout(BodyPart bodyPart, String workoutName) {
+        this.bodyPart = bodyPart;
+        this.workoutName = workoutName;
+    }
+
+    public static Workout of(BodyPart bodyPart, String workoutName) {
+        return new Workout(bodyPart, workoutName);
+    }
+}
