c6e098a57c2b6f7a1339d04ca7a171b10f86f091
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-22 21:28:29 +0900

    Test: 운동 엔티티 생성 테스트 개발

---
+++ src/test/java/com/notifit/domain/workout/entity/WorkoutTest.java
@@ -0,0 +1,25 @@
+package com.notifit.domain.workout.entity;
+
+import com.notifit.domain.workout.entity.enums.BodyPart;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class WorkoutTest {
+
+    @Test
+    @DisplayName("운동 엔티티를 생성한다.")
+    void workoutTest() {
+        // given
+        BodyPart bodyPart = BodyPart.CHEST;
+        String workoutName = "benchPress";
+
+        // when
+        Workout workout = Workout.of(bodyPart, workoutName);
+
+        // then
+        assertThat(workout.getBodyPart()).isEqualTo(bodyPart);
+        assertThat(workout.getWorkoutName()).isEqualTo(workoutName);
+    }
+}
