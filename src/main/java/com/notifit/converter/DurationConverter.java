12510e6b15b8693b9d0bcc15af45bc1328feb9e5
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-25 13:12:46 +0900

    Feat: Duration <--> Long Converter 개발

---
+++ src/main/java/com/notifit/converter/DurationConverter.java
@@ -0,0 +1,20 @@
+package com.notifit.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.time.Duration;
+
+@Converter
+public class DurationConverter implements AttributeConverter<Duration, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(Duration duration) {
+        return duration == null ? null : duration.getSeconds();
+    }
+
+    @Override
+    public Duration convertToEntityAttribute(Long dbData) {
+        return dbData == null ? null : Duration.ofSeconds(dbData);
+    }
+}
