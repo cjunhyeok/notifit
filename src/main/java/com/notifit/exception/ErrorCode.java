197fb92ba7bcc863a8b141900369ec5dc0f2f91d
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-27 23:23:36 +0900

    Feat: 회원 중복 예외코드 추가

--- src/main/java/com/notifit/exception/ErrorCode.java
+++ src/main/java/com/notifit/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    EMPTY_USERNAME(HttpStatus.BAD_REQUEST, "ID 가 비어있습니다.");
+    EMPTY_USERNAME(HttpStatus.BAD_REQUEST, "ID 가 비어있습니다."),
+    DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "중복된 ID 입니다."),
+    ;
 
     private final HttpStatus status;
     private final String errorMessage;
