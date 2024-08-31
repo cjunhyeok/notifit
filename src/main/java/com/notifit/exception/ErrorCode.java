6fb48968f3e7dfc200e60683a9ddd8a68c40c811
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-31 13:33:21 +0900

    Feat: 사용자 아이디 / 비밀번호 가 일치하지 않을 시 발생하는 예외코드 추가

--- src/main/java/com/notifit/exception/ErrorCode.java
+++ src/main/java/com/notifit/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
 
     EMPTY_USERNAME(HttpStatus.BAD_REQUEST, "ID 가 비어있습니다."),
     DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "중복된 ID 입니다."),
+    USERNAME_NOT_MATCH(HttpStatus.BAD_REQUEST, "ID 가 일치하지 않습니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     ;
 
     private final HttpStatus status;
