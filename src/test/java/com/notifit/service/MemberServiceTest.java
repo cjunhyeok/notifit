bcbf9b8f1def64ff8da3f5fd60d368117ef55d48
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-31 19:21:52 +0900

    Test: 회원가입 시 사용자 ID 가 중복된 경우 예외가 발생하는 테스트 개발

--- src/test/java/com/notifit/service/MemberServiceTest.java
+++ src/test/java/com/notifit/service/MemberServiceTest.java
@@ -2,7 +2,10 @@ package com.notifit.service;
 
 import com.notifit.controller.dtos.member.JoinRequest;
 import com.notifit.entity.member.Member;
+import com.notifit.exception.CustomException;
+import com.notifit.exception.ErrorCode;
 import com.notifit.repository.MemberRepository;
+import org.assertj.core.api.AbstractObjectAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +27,7 @@ public class MemberServiceTest {
     @DisplayName("회원 정보를 이용해 회원가입을 진행한다.")
     void joinTest() {
         // given
-        JoinRequest request = JoinRequest
-                .builder()
-                .username("username")
-                .password("password")
-                .name("name")
-                .phoneNumber("010-1234-5678")
-                .isTermOfUseCheck(true)
-                .isPrivacyCheck(true)
-                .build();
+        JoinRequest request = createJoinRequest("username", "password");
 
         // when
         Long savedMemberId = memberService.join(request);
@@ -40,5 +35,36 @@ public class MemberServiceTest {
         // then
         Member findMember = memberRepository.findById(savedMemberId).get();
         assertThat(findMember.getId()).isEqualTo(savedMemberId);
+    }
+
+    @Test
+    @DisplayName("회원가입 시 사용자 ID 가 중복된 경우 예외가 발생한다.")
+    void joinDuplicatedTest() {
+        // given
+        JoinRequest request = createJoinRequest("username", "password");
+        memberService.join(request);
+        JoinRequest joinRequest = createJoinRequest("username", "password");
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> memberService.join(joinRequest))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.DUPLICATED_USERNAME);
+        });
+    }
+
+    private JoinRequest createJoinRequest(String username, String password) {
+        return JoinRequest
+                .builder()
+                .username(username)
+                .password(password)
+                .name("name")
+                .phoneNumber("010-1234-5678")
+                .isTermOfUseCheck(true)
+                .isPrivacyCheck(true)
+                .build();
     }
 }
