9214782f32e12f6ef4a2bb05c9972b2bd1c75fcd
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-08-31 13:23:13 +0900

    Feat: 회원 ID 를 이용해 회원 정보를 조회하는 쿼리 개발

--- src/main/java/com/notifit/repository/MemberRepository.java
+++ src/main/java/com/notifit/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.notifit.repository;
 import com.notifit.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
