1f2d28a3b6ee061cafc0969850c5b974f07e801f
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-09-03 18:39:08 +0900

    Feat: login api 개발

--- src/main/java/com/notifit/controller/MemberController.java
+++ src/main/java/com/notifit/controller/MemberController.java
@@ -3,7 +3,11 @@ package com.notifit.controller;
 import com.notifit.controller.dtos.Result;
 import com.notifit.controller.dtos.member.JoinRequest;
 import com.notifit.controller.dtos.member.JoinResponse;
+import com.notifit.controller.dtos.member.LoginRequest;
 import com.notifit.service.MemberService;
+import com.notifit.service.utils.SessionConst;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,5 +27,17 @@ public class MemberController {
         Long savedMemberId = memberService.join(request);
 
         return new Result<>(new JoinResponse(savedMemberId), "회원가입이 완료됐습니다.");
+    }
+
+    @PostMapping("/api/login")
+    public Result login(@RequestBody LoginRequest request,
+                        HttpServletResponse response) {
+
+        String sessionId = memberService.login(request);
+
+        Cookie sessionCookie = new Cookie(SessionConst.SESSION_ID, sessionId);
+        response.addCookie(sessionCookie);
+
+        return new Result(true, "로그인이 완료되었습니다.");
     }
 }
