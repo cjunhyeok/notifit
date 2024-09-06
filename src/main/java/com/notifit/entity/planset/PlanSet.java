e32d676a63f13c66c8203887f8cabda6e2027910
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-09-06 21:57:29 +0900

    Feat: 운동루틴, 운동세트를 연결하는 다대다 태이블 개발

---
+++ src/main/java/com/notifit/entity/planset/PlanSet.java
@@ -0,0 +1,36 @@
+package com.notifit.entity.planset;
+
+import com.notifit.entity.workoutroutine.WorkoutRoutine;
+import com.notifit.entity.workoutset.WorkoutSet;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlanSet {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "plan_set_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_routine_id")
+    private WorkoutRoutine workoutRoutine;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_set_id")
+    private WorkoutSet workoutSet;
+
+    private PlanSet(WorkoutRoutine workoutRoutine, WorkoutSet workoutSet) {
+        this.workoutRoutine = workoutRoutine;
+        this.workoutSet = workoutSet;
+    }
+
+    public PlanSet of(WorkoutRoutine workoutRoutine, WorkoutSet workoutSet) {
+        return new PlanSet(workoutRoutine,  workoutSet);
+    }
+}
