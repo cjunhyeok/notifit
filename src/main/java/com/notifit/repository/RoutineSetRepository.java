c5e80e1af451c3951984123b3fe1dbc3b9e1cfa8
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-09-09 19:25:11 +0900

    Feat: 운동루틴 운동세트 리포지토리 개발

---
+++ src/main/java/com/notifit/repository/RoutineSetRepository.java
@@ -0,0 +1,7 @@
+package com.notifit.repository;
+
+import com.notifit.entity.routineset.RoutineSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutineSetRepository extends JpaRepository<RoutineSet, Long> {
+}
