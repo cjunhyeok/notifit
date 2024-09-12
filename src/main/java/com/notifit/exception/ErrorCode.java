ff9a4326c2143a1975c11c01491f4fed2de41ab9
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-09-12 19:48:13 +0900

    Feat: 로그인이 안됐을 경우 예외코드 추가

--- src/main/java/com/notifit/exception/ErrorCode.java
+++ src/main/java/com/notifit/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "중복된 ID 입니다."),
     USERNAME_NOT_MATCH(HttpStatus.BAD_REQUEST, "ID 가 일치하지 않습니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    USER_NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
     ;
 
     private final HttpStatus status;
